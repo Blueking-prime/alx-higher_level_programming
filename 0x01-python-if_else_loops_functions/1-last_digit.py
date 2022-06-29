2ff01b641f5a7eb019dba7d822d2d1f95600432b
Nnamdi Anyanwu <blueking189@gmail.com>
2022-06-29 01:32:35 +0100

    Error fixes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,16 @@ number = random.randint(-10000, 10000)
 if abs(number) % 10 == 0:
     print(f"Last digit of {number} is {abs(number) % 10} and is 0")
 if abs(number) % 10 > 5:
-    print(f"Last digit of {number} is {abs(number) % 10}", end='')
-    print(" and is greater than 5")
+    if number > 0:
+        print(f"Last digit of {number} is {abs(number) % 10}", end='')
+        print(" and is greater than 5")
+    else:
+        print(f"Last digit of {number} is -{abs(number) % 10}", end='')
+        print(" and is less than 6 and not 0")
 if abs(number) % 10 < 6 and abs(number) % 10 != 0:
-    print(f"Last digit of {number} is {abs(number) % 10}", end='')
-    print(" and is less than 6 and not 0")
+    if number > 0:
+        print(f"Last digit of {number} is {abs(number) % 10}", end='')
+        print(" and is less than 6 and not 0")
+    else:
+        print(f"Last digit of {number} is -{abs(number) % 10}", end='')
+        print(" and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) <= 122 and ord(str[i]) >= 97:
-            print(chr(ord(str[i]) - 32), end='' if i < len(str) - 1 else '\n')
+            j = chr(ord(str[i])) - 32
+            print("{}".format(j), end='' if i < len(str) - 1 else '\n')
         else:
-            print(str[i], end='' if i < len(str) - 1 else '\n')
+            print("{}".format(str[i]), end='' if i < len(str) - 1 else '\n')
