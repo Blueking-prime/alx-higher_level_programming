16e5460c5cb1d88ffc692205b50847a5713afb57
Nnamdi Anyanwu <blueking189@gmail.com>
2022-06-30 19:24:48 +0100

    Update 2-args.py
    Whitespace changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
     from sys import argv, exit
 
     j = len(argv)
-    
+
     if j == 1:
         print("0 arguments.")
         exit()
 
-    print(f"{j- 1} argument", end=':\n' if j == 2 else 's:\n')
+    print(f"{j - 1} argument", end=':\n' if j == 2 else 's:\n')
     for i in range(j):
         if i == 0:
             continue
