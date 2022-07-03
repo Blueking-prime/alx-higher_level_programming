30f09ddc7dec0efa4d3fbb28f9684ec619099fb7
Nnamdi Anyanwu <blueking189@gmail.com>
2022-07-03 17:09:13 +0100

    Bug Fixes

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,18 +5,20 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     j = 0
     for i in tuple_a:
-        if i is None:
-            l_a[j] = 0
-        else:
-            l_a[j] = i
-        j += 1
+        if j < 2:
+            if i is None:
+                l_a[j] = 0
+            else:
+                l_a[j] = i
+            j += 1
 
     j = 0
     for i in tuple_b:
-        if i is None:
-            l_b[j] = 0
-        else:
-            l_b[j] = i
-        j += 1
+        if j < 2:
+            if i is None:
+                l_b[j] = 0
+            else:
+                l_b[j] = i
+            j += 1
 
     return l_a[0] + l_b[0], l_a[1] + l_b[1]
