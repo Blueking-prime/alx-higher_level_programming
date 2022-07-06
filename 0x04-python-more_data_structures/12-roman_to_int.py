06e7aba221cd9383bf43ee426e7bc04250492639
Nnamdi Anyanwu <blueking189@gmail.com>
2022-07-06 21:27:11 +0100

    Fix tests

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
 
     new_list = []
     roman_converts = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000}
+            'C': 100, 'D': 500, 'M': 1000}
     for i in roman_string:
         new_list.append(i)
     k = 0
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if not a_dictionary:
-        return None
-
     new_dictionary = {x: a_dictionary[x] * 2 for x in a_dictionary}
 
     return new_dictionary
