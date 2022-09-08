85ca71eb93909f5da11dd45c10620178eea2c697
Nnamdi Anyanwu <blueking189@gmail.com>
2022-09-08 04:26:34 +0100

    bug tests

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,6 +3,9 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
            IDENTIFIED BY 'user_0d_1_pwd';
 
+-- Save changes
+FLUSH PRIVILEGES;
+
 -- Grant permissions
 GRANT ALL PRIVILEGES ON *.*
    TO 'user_0d_1'@'localhost'
